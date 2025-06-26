5db4be2c0d20a20082b6bb78d67398822be54e7d
Egidio Carbone <egidiocarbone@192.168.1.57>
2025-06-26 11:08:26 +0200

    Initial commit of GTM Analyzer React project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1a365d",
+        accent: "#FF6B35"
+      }
+    }
+  },
+  plugins: []
+};
